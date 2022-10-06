4dcb4182a5cd5592d2f58ef44e629d490b0c4868
namikguseinoff <namik.guseynov@inbox.ru>
2022-10-06 15:21:20 +0400

    update

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title')
+    Home | FebruaryReadings
+@endsection
+
+@section('content')
+<div>
+    @include('inc.header')
+
+
+    @include('inc.footer')
+</div>
+@endsection
---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,36 @@
+<header class="p-3 text-bg-dark">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-3 text-secondary">Home</a></li>
+          <li><a href="#" class="nav-link px-3 text-white">Committes</a></li>
+          <li><a href="#" class="nav-link px-3 text-white">Invited Speakers</a></li>
+          <li><a href="#" class="nav-link px-3 text-white">Topics</a></li>
+          <li><a href="#" class="nav-link px-3 text-white">Registration</a></li>
+          <li><a href="#" class="nav-link px-3 text-white">Program</a></li>
+          <li><a href="#" class="nav-link px-3 text-white">Particificant</a></li>
+          <li><a href="#" class="nav-link px-3 text-white">Contact</a></li>
+          <li><a href="#" class="nav-link px-3 text-white">Poster</a></li>
+
+        </ul>
+
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+            <select name="language-picker-select" id="language-picker-select">
+                <option lang="de" value="deutsch">Deutsch</option>
+                <option lang="en" value="english" selected>English</option>
+                <option lang="fr" value="francais">Français</option>
+                <option lang="it" value="italiano">Italiano</option>
+                <!-- other language options -->
+              </select>
+        </form>
+
+        
+      </div>
+    </div>
+</header>
+
+
